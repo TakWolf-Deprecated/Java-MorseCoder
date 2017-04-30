965f0d3086a60eae0e1d909451e83b33fe65f6e4
TakWolf <takwolf@foxmail.com>
2017-05-01 05:04:41 +0800

    代码整理

--- README.md
+++ README.md
@@ -1,10 +1,48 @@
-# Java-MorseCoder #
+# Java - MorseCoder #
 
 [![Build Status](https://travis-ci.org/TakWolf/Java-MorseCoder.svg?branch=master)](https://travis-ci.org/TakWolf/Java-MorseCoder)
 [![Download](https://api.bintray.com/packages/takwolf/maven/Java-MorseCoder/images/download.svg)](https://bintray.com/takwolf/maven/Java-MorseCoder/_latestVersion)
 [![License](https://img.shields.io/github/license/TakWolf/Java-MorseCoder.svg?style=flat)](http://www.apache.org/licenses/LICENSE-2.0)
 
-实现上，参考了该项目：[https://github.com/hustcc/xmorse](https://github.com/hustcc/xmorse)
+A Java implementation for Morse Code translation. Use non-standard way to support Unicode.
+
+摩尔斯电码翻译器的一个 Java 语言实现。使用了非标准方式支持 Unicode 编码。
+
+## 关于摩尔斯电码 ##
+
+请参考：[维基百科：摩尔斯电码](https://zh.wikipedia.org/wiki/%E6%91%A9%E5%B0%94%E6%96%AF%E7%94%B5%E7%A0%81)
+
+## 用法 ##
+
+### Gradle ###
+
+```
+dependencies {
+    compile 'com.takwolf:morse-coder:0.0.1'
+}
+
+repositories {
+    jcenter()
+}
+```
+
+### Java ###
+
+```
+MorseCoder morseCoder = new MorseCoder();
+morseCoder.encode("Hello World!");
+morseCoder.decode("...././.-../.-../---/-....-/.--/---/.-./.-../-../-.-.--/");
+```
+
+## 实现 ##
+
+### 标准字典 ###
+
+TODO
+
+### 其他 Unicode 字符 ###
+
+TODO
 
 ## Author ##
 
--- morse-coder/src/main/java/com/takwolf/morsecoder/MorseCoder.java
+++ morse-coder/src/main/java/com/takwolf/morsecoder/MorseCoder.java
@@ -7,8 +7,8 @@ import java.util.Map;
 
 public final class MorseCoder {
 
-    private static final Map<Integer, String> alphabets = new HashMap<>();
-    private static final Map<String, Integer> dictionaries = new HashMap<>();
+    private static final Map<Integer, String> alphabets = new HashMap<>();    // code point -> morse
+    private static final Map<String, Integer> dictionaries = new HashMap<>(); // morse -> code point
 
     private static void registeMorse(Character abc, String dict) {
         alphabets.put(Integer.valueOf(abc), dict);
@@ -17,63 +17,63 @@ public final class MorseCoder {
 
     static {
         // Letters
-        registeMorse('A', "01");      // A
-        registeMorse('B', "1000");    // B
-        registeMorse('C', "1010");    // C
-        registeMorse('D', "100");     // D
-        registeMorse('E', "0");       // E
-        registeMorse('F', "0010");    // F
-        registeMorse('G', "110");     // G
-        registeMorse('H', "0000");    // H
-        registeMorse('I', "00");      // I
-        registeMorse('J', "0111");    // J
-        registeMorse('K', "101");     // K
-        registeMorse('L', "0100");    // L
-        registeMorse('M', "11");      // M
-        registeMorse('N', "10");      // N
-        registeMorse('O', "111");     // O
-        registeMorse('P', "0110");    // P
-        registeMorse('Q', "1101");    // Q
-        registeMorse('R', "010");     // R
-        registeMorse('S', "000");     // S
-        registeMorse('T', "1");       // T
-        registeMorse('U', "001");     // U
-        registeMorse('V', "0001");    // V
-        registeMorse('W', "011");     // W
-        registeMorse('X', "1001");    // X
-        registeMorse('Y', "1011");    // Y
-        registeMorse('Z', "1100");    // Z
+        registeMorse('A', "01");
+        registeMorse('B', "1000");
+        registeMorse('C', "1010");
+        registeMorse('D', "100");
+        registeMorse('E', "0");
+        registeMorse('F', "0010");
+        registeMorse('G', "110");
+        registeMorse('H', "0000");
+        registeMorse('I', "00");
+        registeMorse('J', "0111");
+        registeMorse('K', "101");
+        registeMorse('L', "0100");
+        registeMorse('M', "11");
+        registeMorse('N', "10");
+        registeMorse('O', "111");
+        registeMorse('P', "0110");
+        registeMorse('Q', "1101");
+        registeMorse('R', "010");
+        registeMorse('S', "000");
+        registeMorse('T', "1");
+        registeMorse('U', "001");
+        registeMorse('V', "0001");
+        registeMorse('W', "011");
+        registeMorse('X', "1001");
+        registeMorse('Y', "1011");
+        registeMorse('Z', "1100");
         // Numbers
-        registeMorse('0', "11111");   // 0
-        registeMorse('1', "01111");   // 1
-        registeMorse('2', "00111");   // 2
-        registeMorse('3', "00011");   // 3
-        registeMorse('4', "00001");   // 4
-        registeMorse('5', "00000");   // 5
-        registeMorse('6', "10000");   // 6
-        registeMorse('7', "11000");   // 7
-        registeMorse('8', "11100");   // 8
-        registeMorse('9', "11110");   // 9
+        registeMorse('0', "11111");
+        registeMorse('1', "01111");
+        registeMorse('2', "00111");
+        registeMorse('3', "00011");
+        registeMorse('4', "00001");
+        registeMorse('5', "00000");
+        registeMorse('6', "10000");
+        registeMorse('7', "11000");
+        registeMorse('8', "11100");
+        registeMorse('9', "11110");
         // Punctuation
-        registeMorse('.', "010101");  // Full stop
-        registeMorse(',', "110011");  // Comma
-        registeMorse('?', "001100");  // Question mark
-        registeMorse('\'', "011110"); // Apostrophe
-        registeMorse('!', "101011");  // Exclamation mark
-        registeMorse('/', "10010");   // Slash
-        registeMorse('(', "10110");   // Left parenthesis
-        registeMorse(')', "101101");  // Right parenthesis
-        registeMorse('&', "01000");   // Ampersand
-        registeMorse(':', "111000");  // Colon
-        registeMorse(';', "101010");  // Semicolon
-        registeMorse('=', "10001");   // Equal sign
-        registeMorse('+', "01010");   // Plus sign
-        registeMorse('-', "100001");  // Hyphen1minus
-        registeMorse('_', "001101");  // Low line
-        registeMorse('"', "010010");  // Quotation mark
-        registeMorse('$', "0001001"); // Dollar sign
-        registeMorse('@', "011010");  // At sign
-        registeMorse(' ', "100001");  // Space
+        registeMorse('.', "010101");
+        registeMorse(',', "110011");
+        registeMorse('?', "001100");
+        registeMorse('\'', "011110");
+        registeMorse('!', "101011");
+        registeMorse('/', "10010");
+        registeMorse('(', "10110");
+        registeMorse(')', "101101");
+        registeMorse('&', "01000");
+        registeMorse(':', "111000");
+        registeMorse(';', "101010");
+        registeMorse('=', "10001");
+        registeMorse('+', "01010");
+        registeMorse('-', "100001");
+        registeMorse('_', "001101");
+        registeMorse('"', "010010");
+        registeMorse('$', "0001001");
+        registeMorse('@', "011010");
+        registeMorse(' ', "100001");
     }
 
     private final char dit; // short mark or dot
